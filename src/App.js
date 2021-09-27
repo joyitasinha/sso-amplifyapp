1f1a30338ae63ff252ed06db608545cebb33636f
Joyita Sinha <joyita@evolphin.com>
2021-09-27 11:48:05 +0300

    Committing changes for SSO

--- src/App.js
+++ src/App.js
@@ -1,14 +1,77 @@
-import React from 'react';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>We now have Auth!</h1>
+//       </header>
+//       <AmplifySignOut />
+//     </div>
+//   );
+// }
+
+// export default withAuthenticator(App);
+
+import React, { useEffect, useState } from 'react';
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import awsconfig from './aws-exports';
 import logo from './logo.svg';
 import './App.css';
 
+Amplify.configure(awsconfig);
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+        case 'cognitoHostedUI':
+          getUser().then(userData => setUser(userData));
+          break;
+        case 'signOut':
+          setUser(null);
+          break;
+        case 'signIn_failure':
+        case 'cognitoHostedUI_failure':
+          console.log('Sign in failure', data);
+          break;
+      }
+    });
+
+    getUser().then(userData => setUser(userData));
+  }, []);
+
+  function getUser() {
+    return Auth.currentAuthenticatedUser()
+      .then(userData => userData)
+      .catch(() => console.log('Not signed in'));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
+    <div style={{textAlign: 'center'}}>
+      <p>User: {user ? JSON.stringify(user.attributes) : 'None'}</p>
+      {user ? (
+        <div className="App">
+               <header>
+                 <img src={logo} className="App-logo" alt="logo" />
+                 <h1>We now have Auth!</h1>
+               </header>
+               <button onClick={() => Auth.signOut()}>Sign Out</button>
+               {/* <AmplifySignOut /> */}
+        </div>
+        
+      ) : (
+        <div>
+          <button className="App-button1" onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Sign in with Google</button>
+        </div>
+      )}
     </div>
   );
 }
